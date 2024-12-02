7634fef77bc3efabace9b41e1bce00dc3f8cd27d
kj2k09 <kj2k09@gmail.com>
2024-12-01 21:59:59 -0500

    Create Robot.java

---
+++ Robot.java
@@ -0,0 +1,8 @@
+public class Robot {
+  public static void main(String args[]) {
+private Command drive(double vLeft, double vRight) {
+    return run(() -> drive(vLeft.getAsDouble(), vRight.getAsDouble()));
+      Drive drive = new Drive();
+  }
+}
+}
